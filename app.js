57cfbfcc2d8bf9834ee63de25c30bdf0754978d3
ama <amatsu.t.aa@m.titech.ac.jp>
2018-08-21 15:00:20 +0900

    add cron

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const CheerioHttpcli = require('cheerio-httpcli');
 
 const key3dhDate = datastore.key(['3dh', 'date']);
 const key3dhCardlist = datastore.key(['3dh', 'cardlist']);
-const cachedDate = datastore.get(key3dhDate).then((es) => new Date(es[0].date)).catch((e) => new Date());
+const cachedDate = datastore.get(key3dhDate).then((es) => new Date(es[0].date)).catch((e) => new Date(0));
 const cardlist = datastore.get(key3dhCardlist).then((es) => es[0].cardlist).catch((e) => []);
 
 const {
@@ -45,40 +45,65 @@ function normalizeCardName(name) {
 	return name.toLowerCase().replace(/[ \/]a/g, '-').replace(/[',]/g, '');
 }
 
-async function fetch3dhTable() {
-	//let {$, response, body} = await CheerioHttpcli.fetch(goatbots3dhURI);
-	//({$, response, body} = await $('form').submit());
-	let {
-		$,
-		response,
-		body
-	} = await CheerioHttpcli.fetch('http://localhost:8000/3dh_july_2018.html');
-	return save3dhTable(
-		$('tr').toArray().map((e) =>
-			new CardWithPrice(
-				$($('td', e)[0]).text(),
-				1,
-				parseFloat($($('td', e)[1]).text()),
-				$($('td', e)[3]).text(),
-				$($('td', e)[2]).text(),
-				$($('td', e)[4]).text(),
-				$($('td', e)[5]).text()
-			)
-		),
-		new Date(2018, 6, 20)
+async function fetch3dhTable(submitPromise) {
+	//({$, response, body} = await submitPromise;
+	let {$, response, body} = await CheerioHttpcli.fetch('http://localhost:8000/3dh_july_2018.html');
+	return $('tr').toArray().map((e) =>
+		new CardWithPrice(
+			$($('td', e)[0]).text(),
+			1,
+			parseFloat($($('td', e)[1]).text()),
+			$($('td', e)[3]).text(),
+			$($('td', e)[2]).text(),
+			$($('td', e)[4]).text(),
+			$($('td', e)[5]).text()
+		)
 	);
 }
 
 async function save3dhTable(table, date) {
-	console.log(table.map((c) => c.convertToJSON()));
 	try {
-		await datastore.save({key: key3dhDate, data: {date: date.toISOString()}});
+		await datastore.save({key: key3dhDate, data: {date: date.toJSON()}});
 		await datastore.save({key: key3dhCardlist, data: {cardlist: table.map((c) => c.convertToJSON())}});
 	} catch(e) {
 		console.log(e);
 		return `${e}`;
 	}
 	return `save table ${date}`;
+}
+
+async function update3dhTable(ctx, next) {
+	if(ctx.get('X-Appengine-Cron') != 'true') {
+		ctx.body = 'external access is not allowed';
+		//return;
+	}
+
+	//const {$, response, body} = await CheerioHttpcli.fetch(goatbots3dhURI);
+	//const form = $('form');
+	const [str, mm, dd, yy] = //$('form').parent('#text_left').text();
+		`
+	3 Dollar Highlander
+	3DH or 3 Dollar Highlander is a budget friendly version of Commander. Every deck should cost no more than 3.00 tickets as of the latest revision. For more information about the format or to find fellow players, check out the 3DH Discord Channel.
+	 
+	 
+	Latest Legal Cards
+	Last revision: July 20th 2018
+	
+		
+	
+	`.match(/Last revision: ([^\n]*)\n/i)[1].replace(/Jan|Feb|Mar|Apr|May|June|July|Aug|Sept|Oct|Nov|Dec/, (m) => ({Jan:1, Feb: 2, Mar: 3, Apr: 4, May: 5, June: 6, July: 7, Aug: 8, Sept: 9, Oct: 10, Nov: 11, Dec: 12}[m]).toString()).replace(/th|st|nd|rd/, ' ').match(/(\d*) (\d*) (\d*)/);
+
+	const newDate = new Date(yy, mm - 1, dd);
+
+	if(newDate.getTime() <= (await cachedDate).getTime()) {
+		ctx.body = 'latest';
+		return;
+	}
+
+	//const newTable = await fetch3dhTable(form.submit());
+	const newTable = await fetch3dhTable();
+	ctx.body = save3dhTable(newTable, newDate);
+	return;
 }
 
 async function fetchCardPrice(card, base = new Date()) {
@@ -125,8 +150,8 @@ router
 	.get('/', async (ctx, next) => Send(ctx, '/views' + '/index.html'))
 	.get('/cardlist', async (ctx, next) => ctx.body = await cardlist)
 	.get('/cachedDate', async (ctx, next) => ctx.body = await cachedDate)
-	.get('/calc', async (ctx, next) => ctx.body = await calcDecklist(new Decklist(decodeURIComponent(ctx.request.query.decklist)), (ctx.request.query.fetch && ctx.request.query.fetch == 'true'), (!ctx.request.query.date || ctx.request.query.date == '') ? new Date() : new Date(ctx.request.query.date)))
-	.get('/fetch3dhTable', async (ctx, next) => ctx.body = await fetch3dhTable())
+	.get('/calc', async (ctx, next) => ctx.body = await calcDecklist(new Decklist(decodeURIComponent(ctx.query.decklist)), (ctx.query.fetch && ctx.query.fetch == 'true'), (!ctx.query.date || ctx.query.date == '') ? new Date() : new Date(ctx.query.date)))
+	.get('/update3dhTable', async (ctx, next) => update3dhTable(ctx, next))
 	.get('/:str', async(ctx, next) => Send(ctx, '/views' + '/' + ctx.params.str));
 
 koa
